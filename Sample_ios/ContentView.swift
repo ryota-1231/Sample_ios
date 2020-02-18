96c4afa5f90034e1a11f0c18bfe5e9759e4cbde8
ryota-1231 <rrrrr.1231@gmail.com>
2020-02-18 20:46:01 +0900

    Initial Commit

---
+++ Sample_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample_ios
+//
+//  Created by 青柳竜太 on 2020/02/18.
+//  Copyright © 2020 ryota. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
